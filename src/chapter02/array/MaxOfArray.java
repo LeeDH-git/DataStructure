de0090d3822202200865b1c50b9be923234dcad1
LeeDH-git <djrl02@naver.com>
2020-07-08 21:55:52 +0900

    Initial commit

---
+++ src/chapter02/array/MaxOfArray.java
@@ -0,0 +1,34 @@
+package chapter02.array;
+
+import java.util.Scanner;
+
+public class MaxOfArray {
+
+    static int maxOf(int[] a) {
+        int max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("키의 최댓값을 구한다");
+        System.out.println("사람 수 : ");
+        int num = sc.nextInt();
+
+        int[] height = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("height[" + i + "] :");
+            height[i] = sc.nextInt();
+        }
+        System.out.println("최대값 : " + maxOf(height) + "입니다.");
+    }
+
+}
